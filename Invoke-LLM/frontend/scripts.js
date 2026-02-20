00e4f34f6a53bbfc75a1fe21607654c0f4d1ba67
kailash kumawat <kumawatkai369@gmail.com>
2026-02-20 17:24:24 +0530

    Refactor chat interface: add input handling and message generation functionality

--- Invoke-LLM/frontend/scripts.js
+++ Invoke-LLM/frontend/scripts.js
@@ -1,1 +1,32 @@
-console.log("Hello world!");
+const input = document.querySelector("#input");
+const chatContainer = document.querySelector("#chat-container");
+const askBtn = document.querySelector("#ask");
+
+input.addEventListener("keyup", handleEnter);
+askBtn.addEventListener("click", handleClick);
+
+function generate(text) {
+  const msg = document.createElement("div");
+  msg.className = "my-6 bg-neutral-800 rounded-2xl p-4 max-w-fit ml-auto";
+  msg.textContent = text;
+  chatContainer.append(msg);
+  input.value = "";
+}
+
+function handleClick(e) {
+  const text = input?.value;
+  if (!text) {
+    return;
+  }
+  generate(text);
+}
+
+function handleEnter(e) {
+  if (e.key === "Enter") {
+    const text = input?.value;
+    if (!text) {
+      return;
+    }
+    generate(text);
+  }
+}
