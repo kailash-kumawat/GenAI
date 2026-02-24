94faa382d151dabd5678c5215d146fb5448bf221
kailash kumawat <kumawatkai369@gmail.com>
2026-02-24 10:48:29 +0530

    Implement server-side CORS support and update client-side message handling for chat functionality

--- Invoke-LLM/frontend/scripts.js
+++ Invoke-LLM/frontend/scripts.js
@@ -5,28 +5,54 @@ const askBtn = document.querySelector("#ask");
 input.addEventListener("keyup", handleEnter);
 askBtn.addEventListener("click", handleClick);
 
-function generate(text) {
+// add messages to UI
+async function generate(text) {
   const msg = document.createElement("div");
   msg.className = "my-6 bg-neutral-800 rounded-2xl p-4 max-w-fit ml-auto";
   msg.textContent = text;
   chatContainer.append(msg);
   input.value = "";
+
+  // call server
+  const assistantMessage = await callServer(text);
+
+  const assistantMsgElem = document.createElement("div");
+  assistantMsgElem.className = "max-w-fit";
+  assistantMsgElem.textContent = assistantMessage;
+  chatContainer.append(assistantMsgElem);
 }
 
-function handleClick(e) {
+async function callServer(inputText) {
+  const response = await fetch("http://localhost:3000/chat", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ message: inputText }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error in generating the response");
+  }
+
+  const result = await response.json();
+  return result.message;
+}
+
+// ask button
+async function handleClick(e) {
   const text = input?.value;
   if (!text) {
     return;
   }
-  generate(text);
+  await generate(text);
 }
 
-function handleEnter(e) {
+// enter key
+async function handleEnter(e) {
   if (e.key === "Enter") {
     const text = input?.value;
     if (!text) {
       return;
     }
-    generate(text);
+    await generate(text);
   }
 }
--- Invoke-LLM/server.js
+++ Invoke-LLM/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { generate } from "./chatBot.js";
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
